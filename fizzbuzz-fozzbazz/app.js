c7daff522c62afc56e2aa7ebb83a42a02dc90e84
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-02-04 09:40:37 +0200

    n

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,14 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
+  .factory('counter', ['$window', function(win) {
+   var counter = 0;
+   return function() {
+     counter++;
+     if (counter === 3) {
+       win.alert(counter);
+     }
+   };
+ }])
   .controller('fizzbuzzCtrl', function(){})
   .controller('fozzbazzCtrl', function(){});
